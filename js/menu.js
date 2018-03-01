4907e4d0af8f6641e0ff955c94621030f6bfaa6f
勝木航 <35260777+katsukiwataru@users.noreply.github.com>
2018-03-01 16:47:11 +0900

    tbのメニュー追加

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+;(() => {
+  var Menu__button = document.getElementById('Menu__button');
+  var Menu__list = document.getElementById('Menu__list');
+  var Header__NavigationItem = document.getElementsByClassName('Header__NavigationItem');
+  var isActive = false
+
+  Menu__button.addEventListener('click',function (e) {
+    isActive = !isActive
+    if (isActive) {//ボタンが開いている時
+      Menu__button.className = "Header__Menu active"
+      Menu__list.className = "Header__Navigation active"
+      // Header__NavigationItem.className = "Header__NavigationItem active"
+
+    } else {//ボタンが閉まっている時
+      Menu__button.className = "Header__Menu"
+      Menu__list.className = "Header__Navigation"
+      // Header__NavigationItem.className = "Header__NavigationItem"
+    }
+    e.preventDefault()
+  })
+
+  function closeMenu() {
+     isActive = false
+     Menu__button.className = "Header__Menu"
+     Menu__list.className = "Header__Navigation"
+  }
+  for (var target of Header__NavigationItem) {
+    target.addEventListener("click",closeMenu)
+  }
+
+})()
